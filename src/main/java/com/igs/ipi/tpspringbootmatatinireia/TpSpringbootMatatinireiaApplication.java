0572fac1a155fbed130fc6563fb5f921e13f3cfa
shinlabs <shin_taku@hotmail.com>
2017-12-14 13:34:23 +0100

    first commit

---
+++ src/main/java/com/igs/ipi/tpspringbootmatatinireia/TpSpringbootMatatinireiaApplication.java
@@ -0,0 +1,12 @@
+package com.igs.ipi.tpspringbootmatatinireia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootMatatinireiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootMatatinireiaApplication.class, args);
+	}
+}
